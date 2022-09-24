2e1ca9e65e3db5680ebef905e155ab5b132978a9
CristianR746 <114274126+CristianR746@users.noreply.github.com>
2022-09-24 09:28:57 -0500

    Update setup.py

--- ProyectoProgramacion/setup.py
+++ ProyectoProgramacion/setup.py
@@ -5,7 +5,6 @@ readme = open("./README.md", "r")
 
 setup(
      name='CircuitoProyecto',
-    packages=['circuitoProyecto'],  # this must be the same as the name above
     version='1.0.0',
     description='Esta es la descripcion de mi paquete',
     author= 'Cristian Rivera, Keren pajon, Marcos David',
